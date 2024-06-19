110f91e3d753181b6ff7b106d8c229e009c64378
pray3m <prem.gtm9@gmail.com>
2024-06-19 07:54:25 +0545

    URL Demo

---
+++ url/URLDemo.java
@@ -0,0 +1,56 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class URLDemo {
+
+        // Main driver method
+        @SuppressWarnings("deprecation")
+        public static void main(String[] args)
+                        throws MalformedURLException {
+
+                // Creating a URL with string representation
+                URL url1 = new URL(
+                                "https://www.google.co.in/?gfe_rd=cr&ei=ptYq"
+                                                + "WK26I4fT8gfth6CACg#q=geeks+for+geeks+java");
+
+                // Creating a URL with a protocol,hostname,and path
+                URL url2 = new URL("http", "www.geeksforgeeks.org",
+                                "/jvm-works-jvm-architecture/");
+
+                URL url3 = new URL(
+                                "https://www.google.co.in/search?"
+                                                + "q=gnu&rlz=1C1CHZL_enIN71"
+                                                + "4IN715&oq=gnu&aqs=chrome..69i57j6"
+                                                + "9i60l5.653j0j7&sourceid=chrome&ie=UTF"
+                                                + "-8#q=geeks+for+geeks+java");
+
+                // string representation of the URL
+                System.out.println(url1.toString());
+                System.out.println(url2.toString());
+
+                System.out.println();
+                System.out.println("Different components of the URL3-");
+
+                // protocol for the URL
+                System.out.println("Protocol:- " + url3.getProtocol());
+
+                // hostname of the url
+                System.out.println("Hostname:- " + url3.getHost());
+
+                //  default port
+                System.out.println("Default port:- " + url3.getDefaultPort());
+
+                // query part of URL
+                System.out.println("Query:- " + url3.getQuery());
+
+                // path of URL
+                System.out.println("Path:- " + url3.getPath());
+
+                // file name
+                System.out.println("File:- " + url3.getFile());
+
+                //  reference
+                System.out.println("Reference:- " + url3.getRef());
+
+        }
+}
