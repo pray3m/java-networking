d4619d9e1a64913f11ff7680481c1448cfa38637
pray3m <prem.gtm9@gmail.com>
2024-07-29 08:24:16 +0545

    Implementing different methods in Socket and ServerSocket

---
+++ socket/ClientSocketDemo.java
@@ -0,0 +1,32 @@
+package socket;
+
+// Simple Client Example
+import java.net.*;
+import java.io.*;
+
+public class ClientSocketDemo {
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost", 6666); // Connect to server on localhost, port 8080
+
+        socket.shutdownOutput();
+        boolean noDelay = socket.getTcpNoDelay();
+        System.out.println("Is TCP No Delay ? " + noDelay);
+
+        socket.setTcpNoDelay(true);
+        noDelay = socket.getTcpNoDelay();
+        System.out.println("Is TCP No Delay  (after setting true) ? " + noDelay);
+
+        System.out.println("Is output shutdown ? " + socket.isOutputShutdown());
+
+        // PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        // out.println("Hello, server!");
+
+        // BufferedReader in = new BufferedReader(new
+        // InputStreamReader(socket.getInputStream()));
+        // String messageFromServer = in.readLine();
+        // System.out.println("Server says: " + messageFromServer);
+
+        // socket.setSoTimeout(5000);
+        // socket.close();
+    }
+}
