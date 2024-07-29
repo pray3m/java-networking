8c2aadc2cf85df40d28dd319383ebfffb79ceb29
pray3m <prem.gtm9@gmail.com>
2024-07-29 08:23:44 +0545

    Simple client and servet socket

--- socket/SimpleClient.java
+++ socket/SimpleClient.java
@@ -6,21 +6,10 @@ import java.io.*;
 
 public class SimpleClient {
     public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("localhost", 6666); // Connect to server on localhost, port 8080
-
-        socket.shutdownOutput();
-
-        System.out.println("Is output shutdown ? "+socket.isOutputShutdown());
-
-        // PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-        // out.println("Hello, server!");
-
-
-        // BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        // String messageFromServer = in.readLine();
-        // System.out.println("Server says: " + messageFromServer);
-
-        // socket.setSoTimeout(5000);
-        // socket.close();
+        Socket socket = new Socket("localhost", 8080); // Connect to server on localhost, port 8080
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String messageFromServer = in.readLine();
+        System.out.println("Server says: " + messageFromServer);
+        socket.close();
     }
 }
--- socket/SimpleServer.java
+++ socket/SimpleServer.java
@@ -1,37 +1,16 @@
 package socket;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
+// Simple Server Example
+import java.net.*;
+import java.io.*;
 
 public class SimpleServer {
-    public static void main(String[] args) {
-        try {
-            ServerSocket server = new ServerSocket(6666);
-            System.out.println("Waiting for client request...");
-
-            Socket clientSocket = server.accept();
-            System.out.println("Client connected : " + clientSocket.getInetAddress());
-
-            // send message to client
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            out.println("Hello, client!");
-
-            out.println("Hello, client! 2");
-
-            BufferedReader in = new BufferedReader(new
-            InputStreamReader(clientSocket.getInputStream()));
-            String messageFromClient = in.readLine();
-            System.out.println("client says: " + messageFromClient);
-
-            // server.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(8080); // Server will listen on port 8080
+        System.out.println("Waiting for client...");
+        Socket clientSocket = serverSocket.accept(); // This will wait until a client connects
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+        out.println("Hello, client!");
+        serverSocket.close();
     }
 }
